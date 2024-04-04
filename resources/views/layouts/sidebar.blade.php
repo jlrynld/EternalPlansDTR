c8dc9150cf791ccf56e9234554947e19d017b2ce
EternalPlansOJT <jlbrynld@gmail.com>
2024-04-04 11:58:32 +0800

    reupload

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
     <!-- ====== Sidebar ====== -->
-    <nav class="sidebar">
+    <nav class="sidebar shadow-md">
          <header>
              <div class="image-text">
                 <span class="image">
